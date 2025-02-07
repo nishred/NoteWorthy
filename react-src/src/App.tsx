a3eb2022ab2eadd88d6ca278c05ece8aeeef8e72
nishred <nlankala@horizon.csueastbay.edu>
2025-02-07 12:16:56 -0800

    init

---
+++ react-src/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center">
+      <button className="text-5xl text-purple-900">Roshni</button>
+    </div>
+  );
+}
+
+export default App;
